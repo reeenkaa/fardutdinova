fc2c6ce48c786355c33cdda3d01ec1c534b13e86
reeenkaa <93123395+reeenkaa@users.noreply.github.com>
2021-10-28 11:34:26 +0500

    Create lab-1-3.php

---
+++ lab-1-3.php
@@ -0,0 +1,13 @@
+<b>УПРАЖНЕНИЕ 1-4</b><br> 
+<?php 
+$NUM_E=2.71828; 
+echo 'Число е равно = '.($NUM_E).'<br>'; 
+$num_e1=$NUM_E; 
+echo 'num_e1 = '.($num_e1).' - '.gettype($num_e1).'<br>'; 
+$num_e1="буквы"; 
+echo 'num_e1 = '.($num_e1).' - '.gettype($num_e1).'<br>'; 
+$num_e1=3; 
+echo 'num_e1 = '.($num_e1).' - '.gettype($num_e1).'<br>'; 
+$num_e1=true; 
+echo 'num_e1 = '.($num_e1).' - '.gettype($num_e1).'<br>'; 
+?>
