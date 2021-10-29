ddd228127a308596ebadae9cbcdafdf9da7931e9
reeenkaa <93123395+reeenkaa@users.noreply.github.com>
2021-10-29 14:25:47 +0500

    Update index.php

--- index.php
+++ index.php
@@ -4,3 +4,7 @@
  $d=date("d.m.Y H:i");
  echo($d);
 ?>
+<ul> Практическая работа №1 </ul>
+<li><a href="http://fardutdinova:8080/lab-1-1.php">Упражнение 1-1</a><li>
+<li><a href="http://fardutdinova:8080/lab-1-2.php">Упражнение 1-2</a><li>
+<li><a href="http://fardutdinova:8080/lab-1-3.php">Упражнение 1-3</a><li>
