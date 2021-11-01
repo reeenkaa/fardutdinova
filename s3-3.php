01a5c84a730275dfe61f351c2758e55709e3e868
reeenkaa <93123395+reeenkaa@users.noreply.github.com>
2021-11-01 21:31:04 +0500

    Update s3-3.php

--- s3-3.php
+++ s3-3.php
@@ -1,75 +1,61 @@
-<b>Задача 3.3</b><br>
-<HTML>
-<HEAD> <TITLE> Дурнева А.Д. </TITLE> </HEAD>
-<BODY>
+<HTML> <BODY>
+<H1> Вывод делителей числа N: </H1>
 <FORM method="post" action="<?php print $PHP_SELF ?>">
-Введите число: <INPUT type="text" name="a" size="3">
-<BR> Выбор делителей: <BR> <SELECT NAME="z" SIZE="1">
-<OPTION VALUE="1" SELECTED> четные делители
-<OPTION VALUE="2"> нечетные делители
-<OPTION VALUE="3"> простые делители
-<OPTION VALUE="4"> составные делители
-<OPTION VALUE="5"> все делители
-</SELECT>
-<P> <INPUT type="submit" name="obr" value="Вывести">
+ <P> N: <INPUT type="text" name="n" size="3">
+ <P> Форма обращения: <BR>
+ <SELECT NAME="z" SIZE="1">
+ <OPTION VALUE="1" SELECTED> четные делители
+ <OPTION VALUE="2"> нечетные делители
+ <OPTION VALUE="3"> простые делители
+ <OPTION VALUE="4"> составные делители
+ <OPTION VALUE="5"> все делители
+ </SELECT>
+ <P> <INPUT type="submit" value="Вывести">
 </FORM>
-<?
-if (isset($_POST["obr"])) {
+ <BODY><HTML>
+ <?
+$s1=$_POST["n"];
 switch ($_POST["z"]) {
-// смотрим, чему равна переменная $z
+
 case 1:
-echo "Четные делители числа " . $_POST["a"] . ": ";
-for ($i=1;$i<=$_POST["a"];$i++){
-if($_POST["a"]%$i==0){
-if ($i%2==0)
-echo $i . " ";
+for ($i=1; $i<=$s1; $i++){
+if ($s1%$i==0){
+if ($i%2==0){
+echo "$i <br>";
+}
 }
 }
 break;
+
 case 2:
-echo "Нечетные делители числа " . $_POST["a"] . ": ";
-for ($i=1;$i<=$_POST["a"];$i++){
-if($_POST["a"]%$i==0){
-if ($i%2<>0)
-echo $i . " ";
+for ($i=1; $i<=$s1; $i++){
+if ($s1%$i==0){
+if ($i%2!=0){
+echo "$i <br>";
+}
 }
 }
 break;
+
 case 3:
-if ($_POST["a"]<2)
-echo "Простых делителей заданного числа нет";
-else{
-echo "Простые делители числа " . $_POST["a"] . ": ";
-for ($i=1;$i<=$_POST["a"];$i++){
-if($_POST["a"]%$i==0){
-for ($j = 2;$j < $i;$k++){
-if($i%$j==0) continue 2;
-}
-echo $i . " ";
-}
-}
+for ($i=1; $i<=$s1; $i++){
+if ($s1%$i==0)
+echo "$i <br>";
 }
 break;
 case 4:
-echo "Составные делители числа " . $_POST["a"] . ": ";
-for ($i=1;$i<=$_POST["a"];$i++){
-if($_POST["a"]%$i==0){
-for ($j = 4;$j < $i;$k++){
-if($i%$j==0)
-echo $i . " ";
-continue 2;
-}
-}
+for ($i=1; $i<=$s1; $i++){
+if ($s1%$i==0)
+echo "$i <br>";
 }
 break;
+
 case 5:
-echo "Все делители числа " . $_POST["a"] . ": ";
-for ($i=1;$i<=$_POST["a"];$i++){
-if($_POST["a"]%$i==0)
-echo $i . " ";
+for ($i=1; $i<=$s1; $i++){
+if ($s1%$i==0)
+echo "$i <br>";
 }
 break;
 }
-}
+ echo "*если ответа нет - такого типа делителей нет для данного числа";
 ?>
-</BODY> </HTML>
