e471109551baca9625e65cf62a9cad19f88f975b
reeenkaa <93123395+reeenkaa@users.noreply.github.com>
2021-11-02 19:29:49 +0500

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
  $d=date("d.m.Y H:i");
  echo($d);
 ?>
-<ul> Практическая работа №1 </ul>
-<li><a href="http://fardutdinova:8080/lab-1-1.php">Упражнение 1-1</a><li>
-<li><a href="http://fardutdinova:8080/lab-1-2.php">Упражнение 1-2</a><li>
-<li><a href="http://fardutdinova:8080/lab-1-3.php">Упражнение 1-3</a><li>
-<li><a href="http://fardutdinova:8080/lab-1-5.php">Cамостоятельная задача 1-1</a><li>
+<li><a href="http://fardutdinova:8080/index1.php">Лабораторная работа 1</a><li>
+<li><a href="http://fardutdinova:8080/index2.php">Лабораторная работа 2</a><li>
+<li><a href="http://fardutdinova:8080/index3.php">Лабораторная работа 3</a><li>
