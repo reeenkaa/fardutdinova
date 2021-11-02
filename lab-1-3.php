2594b84d2ee0b6482ac78e389f731bce85cb59d9
reeenkaa <93123395+reeenkaa@users.noreply.github.com>
2021-11-02 19:30:07 +0500

    Update lab-1-3.php

--- lab-1-3.php
+++ lab-1-3.php
@@ -11,3 +11,4 @@ echo 'num_e1 = '.($num_e1).' - '.gettype($num_e1).'<br>';
 $num_e1=true; 
 echo 'num_e1 = '.($num_e1).' - '.gettype($num_e1).'<br>'; 
 ?>
+ <p><a href="http://fardutdinova:8080/index.php ">Главное меню</a></p>
