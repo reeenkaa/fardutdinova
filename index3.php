e3b445158b4b8deb186f060e3c14b63ca0dd833e
reeenkaa <93123395+reeenkaa@users.noreply.github.com>
2021-11-02 19:29:58 +0500

    Create index3.php

---
+++ index3.php
@@ -0,0 +1,14 @@
+<li><a href="http://fardutdinova:8080/index.php">Главное меню</a><li>
+<li><a href="http://fardutdinova:8080/f1.html">Упражнение 3-1</a><li>
+<li><a href="http://fardutdinova:8080/f2.html">Упражнение 3-2</a><li>
+<li><a href="http://fardutdinova:8080/f3.html">Упражнение 3-3</a><li>
+<li><a href="http://fardutdinova:8080/f4.html">Упражнение 3-4</a><li>
+<li><a href="http://fardutdinova:8080/f5.html">Упражнение 3-5</a><li>
+<li><a href="http://fardutdinova:8080/f6.html">Упражнение 3-6</a><li>
+<li><a href="http://fardutdinova:8080/f7.php">Упражнение 3-7</a><li>
+<li><a href="http://fardutdinova:8080/f8.html">Cамостоятельная работа 1</a><li>
+<li><a href="http://fardutdinova:8080/s3-2.php">Cамостоятельная работа 2</a><li>
+<li><a href="http://fardutdinova:8080/s3-3.php">Cамостоятельная работа 3</a><li>
+<li><a href="http://fardutdinova:8080/s3-4.php">Cамостоятельная работа 4</a><li>
+<li><a href="http://fardutdinova:8080/s3-5.php">Cамостоятельная работа 5</a><li>
+<li><a href="http://fardutdinova:8080/s3-6.php">Cамостоятельная работа 6</a><li>
