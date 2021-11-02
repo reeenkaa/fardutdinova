709c47171d42566d53e95949ee59b835063a33bd
reeenkaa <93123395+reeenkaa@users.noreply.github.com>
2021-11-02 19:29:52 +0500

    Create index2.php

---
+++ index2.php
@@ -0,0 +1,7 @@
+<li><a href="http://fardutdinova:8080/index.php">Главное меню</a><li>
+<li><a href="http://fardutdinova:8080/laba-2-1.php">Задание 2-1 </a><li>
+<li><a href="http://fardutdinova:8080/laba-2-2.php">Задание 2-2 </a><li>
+<li><a href="http://fardutdinova:8080/laba-2-3.php">Задание 2-3 </a><li>
+<li><a href="http://fardutdinova:8080/laba-2-4.php">Задание 2-4 </a><li>
+<li><a href="http://fardutdinova:8080/laba-2-5.php">Задание 2-5 </a><li>
+<li><a href="http://fardutdinova:8080/laba-2-6.php">Задание 2-6 </a><li>
