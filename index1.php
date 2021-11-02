1a0b502dced16fc26ac96a9cc0d775a2fd910643
reeenkaa <93123395+reeenkaa@users.noreply.github.com>
2021-11-02 19:29:55 +0500

    Create index1.php

---
+++ index1.php
@@ -0,0 +1,16 @@
+<li><a href="http://fardutdinova:8080/index.php">Главное меню</a><li>
+<li><a href="http://fardutdinova:8080/lab-1-1.php">Упражнение 1-1</a><li>
+<li><a href="http://fardutdinova:8080/lab-1-2.php">Упражнение 1-2</a><li>
+<li><a href="http://fardutdinova:8080/lab-1-3.php">Упражнение 1-3</a><li>
+<li><a href="http://fardutdinova:8080/lab-1-5.php">Cамостоятельная задача 1-1</a><li>
+<li><a href="http://fardutdinova:8080/lab-2-1.php">Упражнение 2-1</a><li>
+<li><a href="http://fardutdinova:8080/lab-2-2.php">Упражнение 2-2</a><li>
+<li><a href="http://fardutdinova:8080/lab-2-3.php">Упражнение 2-3</a><li>
+<li><a href="http://fardutdinova:8080/lab-2-4.php">Упражнение 2-4</a><li>
+<li><a href="http://fardutdinova:8080/lab-2-5.php">Упражнение 2-5</a><li>
+<li><a href="http://fardutdinova:8080/lab-2-6.php">Упражнение 2-6</a><li>
+<li><a href="http://fardutdinova:8080/lab-2-7.php">Упражнение 2-7</a><li>
+<li><a href="http://fardutdinova:8080/lab-2-8.php">Упражнение 2-8</a><li>
+<li><a href="http://fardutdinova:8080/lab-2-9.php">Упражнение 2-9</a><li>
+<li><a href="http://fardutdinova:8080/lab-2-10.php">Cамостоятельная задача 2-1</a><li>
+<li><a href="http://fardutdinova:8080/lab-2-11.php">Cамостоятельная задача 2-2</a><li>
